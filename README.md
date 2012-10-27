9dec864ba9b9a1247b30f569ed793af9848d2d2b
Nova <contact@websanova.com>
2012-10-27 01:15:38 -0400

    Initial commit v1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-jquery-url
-==========
+# $.uri()
 
-$.url() - jQuery url functions
+A jQuery $.uri() method for quickly parsing a uri.
+
+
+## Examples
+
+```html
+http://www.domain.com/path/name?query1=test&silly=willy#test=hash&chucky=cheese
+```
+
+```javascript
+$.uri();            // www.domain.com
+$.uri('domain');    // www.domain.com
+$.uri('tld');       // domain.com
+$.uri('path');      // /path/name/
+$.uri('1');         // path
+$.uri('2');         // name
+$.uri('3');         // (an empty string)
+$.uri('?');         // query1=test&silly=willy
+$.uri('?silly');    // willy
+$.uri('?poo');      // (an empty string)
+$.uri('#');         // test=hash&chucky=cheese
+$.uri('#chucky');   // cheese
+$.uri('#poo');      // (an empty string)
+```
+
+
+## Resources
+
+* [jQuery Plugin Development Boilerplate](http://www.websanova.com/tutorials/jquery/jquery-plugin-development-boilerplate)
+* [The Ultimate Guide to Writing jQuery Plugins](http://www.websanova.com/tutorials/jquery/the-ultimate-guide-to-writing-jquery-plugins)
+
+
+## License
+
+MIT licensed
+
+Copyright (C) 2011-2012 Websanova http://www.websanova.com
---
+++ jquery-uri.js
@@ -0,0 +1,44 @@
+/******************************************
+ * Websanova.com
+ *
+ * Resources for web entrepreneurs
+ *
+ * @author          Websanova
+ * @copyright       Copyright (c) 2012 Websanova.
+ * @license         This websanova jQuery boilerplate is dual licensed under the MIT and GPL licenses.
+ * @link            http://www.websanova.com
+ * @github          http://github.com/websanova/jquery-uri
+ * @version			1.0.0
+ *
+ ******************************************/
+
+jQuery.extend(
+{
+	uri: function(arg)
+	{
+		if(!arg || arg === 'domain') return window.location.hostname;
+		else if(arg === 'tld') return window.location.hostname.split('.').slice(-2).join('.');
+		else if(arg === 'path') return window.location.pathname;
+		else if($.isNumeric(arg)) return window.location.pathname.split('/')[arg] || '';
+		else if(arg[0] === '?' || arg[0] === '#')
+		{
+			var params = window.location.toString(), param = null;
+
+			if(arg[0] === '?') params = (params.split('?')[1] || '').split('#')[0];
+			else if(arg[0] === '#') params = (params.split('#')[1] || '');
+
+			if(!arg[1]) return params;
+
+			arg = arg.substring(1);
+			params = params.split('&');
+
+			for(var i=0,ii=params.length; i<ii; i++)
+			{
+				param = params[i].split('=');
+				if(param[0] === arg) return param[1];
+			}
+		}
+
+		return '';
+	}
+});
